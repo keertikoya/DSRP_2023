38a236947a061af40a44885c15f475eaf01fd4c2
Keerti Koya <koyakeerti@gmail.com>
2023-07-11 13:13:09 -0700

    PAT changes + lab 5 plot

--- notes/GitHubSetup.R
+++ notes/GitHubSetup.R
@@ -5,8 +5,8 @@ usethis::use_git_config(user.name = "Keerti Koya",
 ## create a personal access token (PAT) for authentication
 usethis::create_github_token()
 
-## token: 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
-credentials::set_github_pat("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")
+## token: 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
+credentials::set_github_pat("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")
 
 # git config --global --edit 
   # gets rid of git@github.com: and gets https
