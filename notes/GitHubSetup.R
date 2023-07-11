fee30de4244c713be9253b927ef887490d4bf257
Keerti Koya <koyakeerti@gmail.com>
2023-07-11 09:26:00 -0700

    new token

--- notes/GitHubSetup.R
+++ notes/GitHubSetup.R
@@ -3,7 +3,10 @@ usethis::use_git_config(user.name = "Keerti Koya",
                         user.email = "koyakeerti@gmail.com")
 
 ## create a personal access token (PAT) for authentication
-#usethis::create_github_token()
+usethis::create_github_token()
 
-## token: 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
-credentials::set_github_pat("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")
+## token: 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
+credentials::set_github_pat("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")
+
+# git config --global --edit 
+  # gets rid of git@github.com: and gets https
